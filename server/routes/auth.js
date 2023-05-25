acadb0610caea90151bb8c64cf0f05399535f2c2
FedeNicoletti <federiconicoletti@outlook.com>
2023-05-25 19:34:47 -0300

    Update Auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { login } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js"; // Importar la función register
 
 const router = express.Router();
 
 router.post("/login", login);
+router.post("/register", register); // Agregar esta línea para la ruta /auth/register
 
 export default router;
